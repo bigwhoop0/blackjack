a043d6cd0601d74ade8df1536900c0c77f55e40c
bigwhoop <gael.bocognani@gmail.com>
2023-02-21 18:31:31 +0100

    classe figure terminee

---
+++ classes/Figure.js
@@ -0,0 +1,20 @@
+class Figure {
+  static AS = new Figure("As", 1);
+  static DEUX = new Figure("2", 2);
+  static TROIS = new Figure("3", 3);
+  static QUATRE = new Figure("4", 4);
+  static CINQ = new Figure("5", 5);
+  static SIX = new Figure("6", 6);
+  static SEPT = new Figure("7", 7);
+  static HUIT = new Figure("8", 8);
+  static NEUF = new Figure("9", 9);
+  static DIX = new Figure("10", 10);
+  static VALET = new Figure("Valet", 10);
+  static DAME = new Figure("Dame", 10);
+  static ROI = new Figure("Roi", 10);
+
+  constructor(name, valeur) {
+    this.name = name;
+    this.valeur = valeur;
+  }
+}
