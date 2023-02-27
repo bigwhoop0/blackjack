6fe494d69f129acb0bc01e00efaab75e3f79b5b6
bigwhoop <gael.bocognani@gmail.com>
2023-02-27 18:36:10 +0100

    Résolution bug calcul score avec AS dans Main

--- classes/Main.js
+++ classes/Main.js
@@ -6,14 +6,14 @@ class Main {
   }
 
   calculerScore() {
-    console.log("test");
     let score = 0;
     this.listCartes.forEach((carte) => {
       if (typeof carte.figure === "number") score += carte.figure;
-      else if (carte.figure === "As")
-        this.score + 10 <= 21 ? (score += 10) : (score += 1);
-      else score += 10;
+      else if (carte.figure === "As") {
+        score + 10 <= 21 ? (score += 10) : (score += 1);
+      } else score += 10;
     });
+    console.log(score);
     return score;
   }
 }
