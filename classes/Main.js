9567f190e3e7f3d4b9c5f2819aa8765c963cd45c
bigwhoop <gael.bocognani@gmail.com>
2023-03-04 10:44:22 +0100

    Correction score AS

--- classes/Main.js
+++ classes/Main.js
@@ -9,12 +9,11 @@ class Main {
     //calculer score en cours de la main
     let score = 0;
     this.listCartes.forEach((carte) => {
-      if (typeof carte.figure === "number")
-        score +=
-          carte.figure; //Si la figure est de type number, recupère le nombre pour calculer score
+      if (typeof carte.figure === "number") score += carte.figure;
+      //Si la figure est de type number, recupère le nombre pour calculer score
       else if (carte.figure === "As") {
-        //Valeur de 1 ou 10 en fonction de la valeur de la main
-        score + 10 <= 21 ? (score += 10) : (score += 1);
+        //Valeur de 1 ou 11 en fonction de la valeur de la main
+        score + 10 <= 21 ? (score += 11) : (score += 1);
       } else score += 10; //Si ni un As, ni un number la valeur est de 10 points
     });
     return score;
